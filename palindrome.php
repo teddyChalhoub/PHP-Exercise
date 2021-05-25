4be46b8577d961fa92da50dbd13d784b447ac3d1
teddyChalhoub <teddychalhoub0@gmail.com>
2021-05-25 10:42:10 +0300

    designed the palindrom part3 exercise

--- palindrome.php
+++ palindrome.php
@@ -6,14 +6,14 @@ function palindromeCheck($string)
     $stringrev = strrev($string);
 
     if (strcasecmp($string, $stringrev) == 0) {
-        echo "True";
+        echo "<div class='img' style=' background-color: #C4E7BC;'>";
+        echo "<img src='./img/true.png' alt='True' > ";
+        echo "</div>";
     } else {
-        echo "false";
+        echo "<div class='img' style=' background-color: #BC5449;'>";
+        echo "<img src='./img/false.png' alt='False' > ";
+        echo "</div>";
     }
-}
-
-if (isset($_POST["string"])) {
-    palindromeCheck($_POST["string"]);
 }
 
 ?>
@@ -25,17 +25,32 @@ if (isset($_POST["string"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/palindrome.css">
+    <title>Palindrome</title>
 </head>
 
 <body>
-    <form action="./palindrome.php" method="POST">
 
-        <label for="string">String:</label>
-        <input id="string" type="text" name="string"><br><br>
+    <div class="wrapper">
+        <h3>Palindrom Checking </h3>
+        <p>Provide a value to check if the name in reverse stay the same</p>
+        <form action="./palindrome.php" method="POST">
 
-        <button type="submit">submit</button>
+            <label for="string">Value</label>
+            <input id="string" type="text" name="string" value=<?php echo "$_POST[string]"; ?>><br><br>
 
+            <button type="submit">submit</button>
+        </form>
+
+
+        <?php
+
+        if (isset($_POST["string"])) {
+            palindromeCheck($_POST["string"]);
+        }
+
+        ?>
+    </div>
 </body>
 
 </html>
